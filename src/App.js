009f4fb8fb28b57db7eb5ceafb01028c0c5df20e
Jaxon Moffitt <jrmoffit@byu.edu>
2020-06-16 23:01:03 -0500

    created card and centered it

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div class="card">
-      <img src={logo} className="App-logo" alt="logo" style="width:100%" />
-      <div class="container">
-        <h4><b>John Doe</b></h4>
-        <p>Architect & Engineer</p>
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+      <div className="card">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="container">
+          <h4><b>John Doe</b></h4>
+          <p>Architect & Engineer</p>
+        </div>
       </div>
     </div>
   );
