dcb6f38fb433e32e4731b56c24a6165a11bf8a63
Jaxon Moffitt <jrmoffit@byu.edu>
2020-06-17 19:20:43 -0500

    added text styles

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,6 +20,17 @@ function About() {
         <div style={{display: 'flex',  justifyContent:'center'}} >
             <img src={headerImage} className="App-logo align" alt="logo" width='100%' height='100%'/>
         </div>
+        <div style={{display: 'flex',  justifyContent:'center'}} >
+            <h2 className="header-text">Introduction</h2>
+        </div>
+        <div style={{display: 'flex',  justifyContent:'center'}} >
+            <p className="body-text">
+            As April rolls around every year, NFL team general managers and owners begin to sweat for many reasons: How should they approach free agency? How will they afford to re-sign their current players? And perhaps most importantly, who should they select in the upcoming NFL draft? Among a sea of prospects to choose from, it can be impossible at times to determine who has separated themselves from the pack of premier athletes. We decided to tackle the question ourselves and analyze how incoming draft picks’ college statistics and measurements at the NFL Combine could predict their value to their future team. While many teams utilize advanced statistics within their organizational decision-making, we figured few teams had actually built models using the available data to judge which players might have the brightest futures. Using statistical modeling, we would try to identify trends within our past data to see if there were any specific indicators of certain statistics to heavily weigh within a team’s choice of draft pick.
+            </p>
+        </div>
+
+
+
         <div style={{display: 'flex',  justifyContent:'center', height: '500px'}} >
             <p></p>
         </div>
