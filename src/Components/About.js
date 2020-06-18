1d9cfc9d646ed5e77ff37e5f5390f640ed686831
Jaxon Moffitt <jrmoffit@byu.edu>
2020-06-17 20:21:57 -0500

    added methodology block

--- src/Components/About.js
+++ src/Components/About.js
@@ -28,6 +28,14 @@ function About() {
             As April rolls around every year, NFL team general managers and owners begin to sweat for many reasons: How should they approach free agency? How will they afford to re-sign their current players? And perhaps most importantly, who should they select in the upcoming NFL draft? Among a sea of prospects to choose from, it can be impossible at times to determine who has separated themselves from the pack of premier athletes. We decided to tackle the question ourselves and analyze how incoming draft picks’ college statistics and measurements at the NFL Combine could predict their value to their future team. While many teams utilize advanced statistics within their organizational decision-making, we figured few teams had actually built models using the available data to judge which players might have the brightest futures. Using statistical modeling, we would try to identify trends within our past data to see if there were any specific indicators of certain statistics to heavily weigh within a team’s choice of draft pick.
             </p>
         </div>
+        <div style={{display: 'flex',  justifyContent:'center'}} >
+            <h2 className="header-text">Methodology</h2>
+        </div>
+        <div style={{display: 'flex',  justifyContent:'center'}} >
+            <p className="body-text">
+            The definition of a successful player is often a very arbitrary matter within the NFL, so we decided to utilize a well-respected site’s definition: <a href="https://www.pro-football-reference.com/">Pro-Football-Reference.com</a>’s metric of Average Value. Their formula consists of a few different levels, but essentially assigns a value to each player based on the points, yards, and possessions they are responsible for within a game. It serves a similar purpose to the win shares statistic that has become prevalent in some of the other major sports.
+            </p>
+        </div>
 
 
 
